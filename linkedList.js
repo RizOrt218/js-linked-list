b9a492b26912e3ddcfd0d4adf548fc96136a6a20
RizOrt218 <rizhiafalen@gmail.com>
2015-12-07 15:24:33 -1000

    working working still

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,22 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var animals = {};
 
+  return {
+    getHead : function(animals){
+
+    },
+    getTail : function(){
+
+    },
+    add : function(){
+      // var addNewTail = linkedListGenerator();
+      //   addNewTail.add('turtle');
+    },
+    remove : function(){},
+    get : function(){},
+    insert : function(){}
+  };
 }
+
