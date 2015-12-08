62eb59303f385d2bb598deee6f208080d1aefe02
RizOrt218 <rizhiafalen@gmail.com>
2015-12-07 16:00:08 -1000

    add newNode

--- linkedList.js
+++ linkedList.js
@@ -4,22 +4,39 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-  var animals = {};
+  var head = null;
+  var tail = null;
 
   return {
-    getHead : function(animals){
-
+    getHead : function(){
+      return head;
     },
     getTail : function(){
-
+      return tail;
     },
-    add : function(){
-      // var addNewTail = linkedListGenerator();
-      //   addNewTail.add('turtle');
+    add : function(value){
+      var newNode = {
+        value : value,
+        next : null
+      };
+
+      if(head === null && tail === null){
+        head = newNode;
+        tail = newNode;
+      } else {
+        tail.next = newNode;
+        tail = newNode;
+      }
+
+      return newNode;
+
     },
     remove : function(){},
     get : function(){},
     insert : function(){}
   };
+
+
 }
+
 
